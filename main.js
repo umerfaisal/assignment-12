56ddd7dbd7d9c2eb42d9785675685bd0adb63210
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-25 21:53:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+var friendsName = ["Hamza", "Umer", "Huzaifa", "Mubeen", "Maaz"];
+var message = "Would you like to learn some typescript today?";
+friendsName.forEach(function (friendsName) { return console.log(friendsName + " " + message); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+let friendsName = ["Hamza","Umer","Huzaifa","Mubeen","Maaz"]
+let message = "Would you like to learn some typescript today?"
+friendsName.forEach(friendsName => console.log(friendsName+ " " +message));
